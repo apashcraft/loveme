0171bc47a6ccc2bba509fddf135558d37fe4becb
Adam Ashcraft <apashcraft@gmail.com>
2020-02-07 00:39:25 -0500

    Temporarily removed APOD update functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 
-from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.schedulers.background import BackgroundScheduler
 from config import Config
 
-from app.modules.helpers import update_apod
+#from app.modules.helpers import update_apod
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    """
     nasa_key = app.config['NASA_KEY']
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=update_apod, trigger="cron", hour=6, args=[nasa_key])
     scheduler.start()
+    """
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
